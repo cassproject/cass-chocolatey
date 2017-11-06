e8473b303348184738af97dd9ea0b14b8532cf3c
Ronald Ray <fritz.ray@eduworks.com>
2017-11-06 15:14:32 -0800

    Fixed typo.

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -26,4 +26,4 @@ cd ..
 #Ensure IIS is installed and configured.
 echo "DISM.EXE /online /enable-feature /all /featureName:IIS-WebServerRole /featureName:IIS-WebServer /featureName:IIS-CommonHttpFeatures /featureName:IIS-StaticContent /featureName:IIS-RequestFiltering /featureName:IIS-IPSecurity /featureName:IIS-HttpCompressionStatic /featureName:IIS-HttpCompressionDynamic /featureName:IIS-WebServerManagementTools /featureName:IIS-ManagementScriptingTools /featureName:IIS-ManagementService /featureName:IIS-WMICompatibility /featureName:IIS-WebSockets /featureName:IIS-CertProvider /featureName:IIS-ManagementConsole /featureName:IIS-LegacySnapIn"
 echo ""
-echo "You will still have to install AAR, URLRewrite, and a Web Farm. We recommend adding a web farm with server port 8080/8080. The above command will install IIS, then 'choco install iis-aar urlrewrite'."
+echo "You will still have to install AAR, URLRewrite, and a Web Farm. We recommend adding a web farm with server port 8080/8080. The above command will install IIS, then 'choco install iis-arr urlrewrite'."
