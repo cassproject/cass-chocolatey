68cfbeab6a903bb05258884b873b17e5e55f8f74
Fritz Ray <fritz.ray@eduworks.com>
2019-09-19 08:45:46 -0700

    Still not working due to broken dependencies.

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,25 +3,22 @@
 $packageName= 'CASS' # arbitrary name for the package, used in messages
 
 #Install CASS
-mkdir temp -Force
-cd temp
-git clone https://github.com/cassproject/cass -b master
-cd cass
-mvn install
-cd target
 set-service "elasticsearch-service-x64" -startuptype "auto"
 set-service "Tomcat8" -startuptype "auto"
 stop-service "elasticsearch-service-x64"
 stop-service "Tomcat8"
+
+$version = Read-Host -Prompt 'Input the version you would like to install (eg 1.2.24 - You can see a list of versions at https://github.com/cassproject/CASS/releases)'
+
 rm "$env:CATALINA_HOME\webapps\root.war"
+rm "$env:CATALINA_HOME\webapps\cass.war"
 rm "$env:CATALINA_HOME\webapps\ROOT" -Recurse
-cp -Path "$pwd\cass.war" -Destination "$env:CATALINA_HOME\webapps\ROOT.war"
+$url = "https://github.com/cassproject/CASS/releases/download/$version/cass.war"
+$output = "$env:CATALINA_HOME\webapps\ROOT.war"
+Start-BitsTransfer -Source $url -Destination $output
+
 start-service "elasticsearch-service-x64"
 start-service "Tomcat8"
-cd ..
-cd ..
-rm "cass" -Recurse -Force
-cd ..
 
 #Ensure IIS is installed and configured.
 echo "DISM.EXE /online /enable-feature /all /featureName:IIS-WebServerRole /featureName:IIS-WebServer /featureName:IIS-CommonHttpFeatures /featureName:IIS-StaticContent /featureName:IIS-RequestFiltering /featureName:IIS-IPSecurity /featureName:IIS-HttpCompressionStatic /featureName:IIS-HttpCompressionDynamic /featureName:IIS-WebServerManagementTools /featureName:IIS-ManagementScriptingTools /featureName:IIS-ManagementService /featureName:IIS-WMICompatibility /featureName:IIS-WebSockets /featureName:IIS-CertProvider /featureName:IIS-ManagementConsole /featureName:IIS-LegacySnapIn"
